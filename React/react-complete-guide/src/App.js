c85cf6bb9e40a5258d4319e6ee6219382adaa090
nghochi123 <nghochi123@gmail.com>
2020-12-21 10:35:50 +0800

    Learning React First Commit

---
+++ React/react-complete-guide/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  state = {
+    persons: [
+			{ name: 'Max', age: 28},
+			{ name: 'Manu', age: 29},
+			{ name: 'Stephanie', age: 26}
+		]
+  };
+  switchNameHandler = () =>{
+    console.log('Button was clicked');
+  }
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28},
+        { name: event.target.value, age: 29},
+        { name: 'Stephanie', age: 26}
+      ]
+    })
+  }
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app</h1>
+        <button 
+        style={style} 
+        onClick={this.switchNameHandler}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler}
+        changed={this.nameChangedHandler}/>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
+      </div>
+      
+    );
+  };
+}
+
+export default App;
