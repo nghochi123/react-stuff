0a67e60bbfb231376503937ac06f6697d52c3059
nghochi123 <nghochi123@gmail.com>
2020-12-25 16:15:03 +0800

    Non-dynamic input of Burger Ingredients

---
+++ React/burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Auxiliary from '../../hoc/Auxiliary'
+import styles from './Layout.module.css'
+
+const layout = (props) => (
+    <Auxiliary>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={styles.Content}>
+            {props.children}
+        </main>
+    </Auxiliary>
+    
+);
+
+export default layout;
