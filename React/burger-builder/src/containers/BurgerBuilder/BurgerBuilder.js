33a6eb7345387900581e0b9153612a30fee57beb
nghochi123 <nghochi123@gmail.com>
2020-12-25 20:09:13 +0800

    Added functionality to the burger builder: price and adding ingredients

--- React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ React/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 import Aux from'../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 0,
-            bacon: 1,
+            bacon: 0,
             cheese: 0,
             meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const updatedPrice = this.state.totalPrice + priceAddition;
+        this.setState({
+            totalPrice: updatedPrice,
+            ingredients: updatedIngredients
+        });
+    }
+
+    removeIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        if(oldCount !== 0){    
+            const updatedCount = oldCount - 1;
+            const updatedIngredients = {...this.state.ingredients};
+            updatedIngredients[type] = updatedCount;
+            const priceDiff = INGREDIENT_PRICES[type];
+            const updatedPrice = this.state.totalPrice - priceDiff;
+            this.setState({
+                totalPrice: updatedPrice,
+                ingredients: updatedIngredients
+            });
         }
     }
 
@@ -17,7 +54,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Control</div>
+                <BuildControls
+                ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}
+                price={this.state.totalPrice}/>
             </Aux>
 
         );
